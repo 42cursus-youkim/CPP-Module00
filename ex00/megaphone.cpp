75ba39594c9302c9d2022674855d70f528cbbda8
scarf <greenscarf005@gmail.com>
2022-01-30 00:33:11 +0900

    feat: :sparkles: ex00

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+std::string toUpper(std::string str) {
+  std::string res = str;
+
+  for (unsigned i = 0; i < str.length(); i++) {
+    res[i] = std::toupper(str[i]);
+  }
+  return res;
+}
+
+int main(int argc, char* argv[]) {
+  if (argc == 1) {
+    std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+  } else {
+    for (int i = 1; i < argc; i++) {
+      std::cout << toUpper(argv[i]);
+    }
+    std::cout << std::endl;
+  }
+  return (0);
+}
