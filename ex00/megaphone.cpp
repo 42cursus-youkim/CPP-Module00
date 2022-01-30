0eb9e28c2403927d6d6c4e79fe85b1f3a999f60e
scarf <greenscarf005@gmail.com>
2022-01-30 13:26:15 +0900

    refactor: :recycle: endl is used in both cases

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -12,12 +12,11 @@ std::string toUpper(std::string str) {
 
 int main(int argc, char* argv[]) {
   if (argc == 1) {
-    std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+    std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
   } else {
-    for (int i = 1; i < argc; i++) {
+    for (int i = 1; i < argc; i++)
       std::cout << toUpper(argv[i]);
-    }
-    std::cout << std::endl;
   }
+  std::cout << std::endl;
   return (0);
 }
